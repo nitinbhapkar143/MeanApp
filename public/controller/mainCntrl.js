ce9d8b9a340ec0c789d227b733320046dc949543
nitin <nitin 889>
2017-06-07 23:52:43 +0530

    Added logic to check login for each view

--- public/controller/mainCntrl.js
+++ public/controller/mainCntrl.js
@@ -1,18 +1,23 @@
 angular.module('appLoginCntrl',['authService'])
-	.controller('mainCntrl',function (Auth, $location, $timeout) {
+	.controller('mainCntrl',function (Auth, $location, $timeout, $rootScope) {
 		var vm = this;
 
-		if(Auth.isLoggedIn()){
-			Auth.getUser().then(function(data){
-				vm.username = data.data.username;
-				vm.email = data.data.email;
-			});
-		}
-		else{
-			console.log('Ooops! User seems to be logged out...');
-		}
-
-
+		$rootScope.$on('$routeChangeStart',function(){
+			if(Auth.isLoggedIn()){
+				Auth.getUser().then(function(data){
+					vm.username = data.data.username;
+					vm.email = data.data.email;
+					vm.isLoggedIn = true;
+					console.log('Congrats!!! You are now logged in...');
+				});
+			}
+			else{
+				vm.isLoggedIn = false;
+				vm.username = vm.email = '';
+				console.log('Ooops! User seems to be logged out...');
+			}
+		});
+		
 		vm.authenticateUser = function (loginData){
 			vm.loading = true;
 			vm.errorMsg = false;
